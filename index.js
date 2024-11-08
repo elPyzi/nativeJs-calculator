e7b9cf21870c58a805e7f579bd411c0483d6b8a7
Danila <danila.pyzhikov@gmail.com>
2024-11-08 21:39:15 +0300

    Updates

---
+++ index.js
@@ -0,0 +1,8 @@
+import './assets/images/favicon.png';
+import './assets/styles/index.css';
+import './assets/styles/footer.css';
+import './assets/styles/greeting.css';
+import './assets/styles/calculator.css';
+import './assets/styles/convertor.css';
+import './utils/theme/changeTheme';
+import './utils/calculator';
---
+++ utils/theme/changeTheme.js
@@ -0,0 +1,70 @@
+const htmlElement = document.documentElement;
+(function () {
+    const currentTheme = localStorage.getItem('currentTheme');
+    if (currentTheme) {
+        switch (currentTheme) {
+            case 'light': {
+                htmlElement.setAttribute('data-theme', 'light');
+                break;
+            }
+            case 'dark': {
+                htmlElement.setAttribute('data-theme', 'dark');
+                break;
+            }
+            case 'night': {
+                htmlElement.setAttribute('data-theme', 'night');
+                break;
+            }
+            default: {
+                htmlElement.setAttribute('data-theme', 'light');
+                break;
+            }
+        }
+    } else {
+        htmlElement.setAttribute('data-theme', 'light');
+    }
+})();
+
+/**
+ * * Кнопки на изменение цвета
+ */
+
+const LIGHT_BTN = document.querySelector('.light');
+const DARK_BTN = document.querySelector('.dark');
+const NIGHT_BTN = document.querySelector('.night');
+
+LIGHT_BTN.addEventListener('click', (event) => {
+    const isLight = event.target.getAttribute('light-mode') === 'true';
+    if (!isLight) {
+        htmlElement.setAttribute('data-theme', 'light');
+        event.target.setAttribute('light-mode', 'true');
+        DARK_BTN.setAttribute('dark-mode', 'false');
+        NIGHT_BTN.setAttribute('night-mode', 'false');
+
+        localStorage.setItem('currentTheme', 'light');
+    }
+});
+
+DARK_BTN.addEventListener('click', (event) => {
+    const isDark = event.target.getAttribute('dark-mode') === 'true';
+    if (!isDark) {
+        htmlElement.setAttribute('data-theme', 'dark');
+        event.target.setAttribute('dark-mode', 'true');
+        LIGHT_BTN.setAttribute('light-mode', 'false');
+        NIGHT_BTN.setAttribute('night-mode', 'false');
+
+        localStorage.setItem('currentTheme', 'dark');
+    }
+});
+
+NIGHT_BTN.addEventListener('click', (event) => {
+    const isNight = event.target.getAttribute('night-mode') === 'true';
+    if (!isNight) {
+        htmlElement.setAttribute('data-theme', 'night');
+        event.target.setAttribute('night-mode', 'true');
+        DARK_BTN.setAttribute('dark-mode', 'false');
+        LIGHT_BTN.setAttribute('light-mode', 'false');
+
+        localStorage.setItem('currentTheme', 'night');
+    }
+});
